56436edaf74712836943a482ede20d0cc53e4e1b
Julian B <bechti44@googlemail.com>
2023-02-27 18:46:19 +0100

    moved code to apropriate location

--- Chia-Client-API/FullNodeAPI_NS/FullNode_RPC_Client.cs
+++ Chia-Client-API/FullNodeAPI_NS/FullNode_RPC_Client.cs
@@ -84,31 +84,5 @@ namespace Chia_Client_API.FullNodeAPI_NS
             data.Wait();
             return data.Result;
         }
-        /// <summary>
-        /// Show the current network (eg mainnet) and network prefix (eg XCH)
-        /// </summary>
-        /// <returns></returns>
-        public async Task GetAllMempoolItems_Async()
-        {
-            string response = await SendCustomMessage_Async("get_all_mempool_items");
-            GetAllMempoolItems_Response json = JsonSerializer.Deserialize<GetAllMempoolItems_Response>(response);
-            return;
-        }
-        /// <summary>
-        /// finds a coin record in the blockchain. 
-        /// Warning: This is barely useful, since it does not include spent coins!
-        /// </summary>
-        public async Task<GetCoinRecordByName_Response> GetCoinRecordByName_Async(GetCoinRecordByName_RPC name)
-        {
-            string response = await SendCustomMessage_Async("get_coin_record_by_name", name.ToString());
-            GetCoinRecordByName_Response json = JsonSerializer.Deserialize<GetCoinRecordByName_Response>(response);
-            return json;
-        }
-        public async Task<GetCoinRecordsByNames_Response> GetCoinRecordsByNames_Async(GetCoinRecordsByNames_RPC name)
-        {
-            string response = await SendCustomMessage_Async("get_coin_records_by_names", name.ToString());
-            GetCoinRecordsByNames_Response json = JsonSerializer.Deserialize<GetCoinRecordsByNames_Response>(response);
-            return json;
-        }
     }
 }
