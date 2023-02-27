8a945428c5a9112bb17e6f789cb2650756e26329
Julian B <bechti44@googlemail.com>
2023-02-27 18:47:36 +0100

    first preparations for complete full-node implementation - added a lot of objects and rpc's which could already be used for custom requests

---
+++ Chia-Client-API/FullNodeAPI_NS/FullNodeApi.cs
@@ -0,0 +1,58 @@
+﻿
+using CHIA_RPC.FullNode_RPC_NS;
+using CHIA_RPC.Wallet_RPC_NS.Wallet_NS;
+using CHIA_RPC.Wallet_RPC_NS.WalletNode_NS;
+using System.Text.Json;
+
+namespace Chia_Client_API.FullNodeAPI_NS
+{
+    public partial class FullNode_RPC_Client
+    {
+        /// <summary>
+        /// Show the current network (eg mainnet) and network prefix (eg XCH)
+        /// </summary>
+        /// <returns></returns>
+        public async Task<GetNetworkInfo_Response> GetNetworkInfo_Async()
+        {
+            string response = await SendCustomMessage_Async("get_network_info");
+            GetNetworkInfo_Response json = JsonSerializer.Deserialize<GetNetworkInfo_Response>(response);
+            return json;
+        }
+        /// <summary>
+        /// Show the current network (eg mainnet) and network prefix (eg XCH)
+        /// </summary>
+        /// <returns></returns>
+        public GetNetworkInfo_Response GetNetworkInfo_Sync()
+        {
+            Task<GetNetworkInfo_Response> data = Task.Run(() => GetNetworkInfo_Async());
+            data.Wait();
+            return data.Result;
+        }
+        /// <summary>
+        /// Show the current network (eg mainnet) and network prefix (eg XCH)
+        /// </summary>
+        /// <returns></returns>
+        public async Task GetAllMempoolItems_Async()
+        {
+            string response = await SendCustomMessage_Async("get_all_mempool_items");
+            GetAllMempoolItems_Response json = JsonSerializer.Deserialize<GetAllMempoolItems_Response>(response);
+            return;
+        }
+        /// <summary>
+        /// finds a coin record in the blockchain. 
+        /// Warning: This is barely useful, since it does not include spent coins!
+        /// </summary>
+        public async Task<GetCoinRecordByName_Response> GetCoinRecordByName_Async(GetCoinRecordByName_RPC name)
+        {
+            string response = await SendCustomMessage_Async("get_coin_record_by_name", name.ToString());
+            GetCoinRecordByName_Response json = JsonSerializer.Deserialize<GetCoinRecordByName_Response>(response);
+            return json;
+        }
+        public async Task<GetCoinRecordsByNames_Response> GetCoinRecordsByNames_Async(GetCoinRecordsByNames_RPC name)
+        {
+            string response = await SendCustomMessage_Async("get_coin_records_by_names", name.ToString());
+            GetCoinRecordsByNames_Response json = JsonSerializer.Deserialize<GetCoinRecordsByNames_Response>(response);
+            return json;
+        }
+    }
+}
