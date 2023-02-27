d57be5bc2c11e08e5357cddd9594bd76fa1426cc
Julian B <bechti44@googlemail.com>
2023-02-27 13:16:23 +0100

    full node is no longer static. this allows to create multiple clients in parralel and to connect to multiple hosts

--- Chia-Client-API/FullNodeAPI_NS/FullNode_RPC_Client.cs
+++ Chia-Client-API/FullNodeAPI_NS/FullNode_RPC_Client.cs
@@ -4,27 +4,64 @@ using System.Net.Http.Headers;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 
-namespace Chia_Client_API.Clients_NS
+namespace Chia_Client_API.FullNodeAPI_NS
 {
-    public static class FullNode_RPC_Client
+    public partial class FullNode_RPC_Client
     {
-        static FullNode_RPC_Client()
+        public FullNode_RPC_Client(string targetApiAddress = "localhost", int targetApiPort = 8555, string? targetCertificateBaseFolder = null)
         {
-            //initialize http client with proper certificate
+            TargetApiAddress = targetApiAddress;
+            TargetApiPort = targetApiPort;
+            // this also sets the client
+            if (targetCertificateBaseFolder != null)
+            {
+                API_CertificateFolder = targetCertificateBaseFolder;
+            }
+            else
+            {
+                API_CertificateFolder = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+            @".chia\mainnet\config\ssl\");
+            }
+        }
+        private HttpClient _Client { get; set; }
+        /// <summary>
+        /// the address under which the node can be reached. Defaults to localhost (127.0.0.1)
+        /// </summary>
+        public string TargetApiAddress { get; set; }
+        /// <summary>
+        /// the port which should be used. defaults to 8555
+        /// </summary>
+        public int TargetApiPort { get; set; }
+        /// <summary>
+        /// the base folder is the folder where all certificates are contained within subfolders according to chias default structure
+        /// </summary>
+        public string API_CertificateFolder
+        {
+            get { return _API_CertificateFolder; }
+            set { _API_CertificateFolder = value; SetNewCerticifactes(); }
+        }
+        private string _API_CertificateFolder;
+        /// <summary>
+        /// this function creates a new http cliet with the set certificates
+        /// </summary>
+        private void SetNewCerticifactes()
+        {
+            if (_Client != null) _Client.Dispose();
+            // initialize http client with proper certificate
             var handler = new HttpClientHandler();
-            X509Certificate2 privateCertificate = CertificateLoader.GetCertificate(Endpoint.full_node);
+            X509Certificate2 privateCertificate = CertificateLoader.GetCertificate(Endpoint.full_node, _API_CertificateFolder);
             handler.ServerCertificateCustomValidationCallback = (requestMessage, certificate, chain, policyErrors) => true;
             handler.ClientCertificates.Add(privateCertificate);
             _Client = new HttpClient(handler);
         }
-        private static HttpClient _Client;
         /// <summary>
         /// with this function you can execute any RPC against the wallet api. it is internally used by the library
         /// </summary>
         /// <param name="function"></param>
         /// <param name="json"></param>
         /// <returns></returns>
-        public static async Task<string> SendCustomMessage_Async(string function, string json = " { } ")
+        public async Task<string> SendCustomMessage_Async(string function, string json = " { } ")
         {
             using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://" + GlobalVar.API_TargetIP + ":8555/" + function))
             {
@@ -41,7 +78,7 @@ namespace Chia_Client_API.Clients_NS
         /// <param name="function"></param>
         /// <param name="json"></param>
         /// <returns></returns>
-        public static string SendCustomMessage_Sync(string function, string json = " { } ")
+        public string SendCustomMessage_Sync(string function, string json = " { } ")
         {
             Task<string> data = Task.Run(() => SendCustomMessage_Async(function, json));
             data.Wait();
@@ -51,7 +88,7 @@ namespace Chia_Client_API.Clients_NS
         /// Show the current network (eg mainnet) and network prefix (eg XCH)
         /// </summary>
         /// <returns></returns>
-        public async static Task GetAllMempoolItems_Async()
+        public async Task GetAllMempoolItems_Async()
         {
             string response = await SendCustomMessage_Async("get_all_mempool_items");
             GetAllMempoolItems_Response json = JsonSerializer.Deserialize<GetAllMempoolItems_Response>(response);
@@ -61,13 +98,13 @@ namespace Chia_Client_API.Clients_NS
         /// finds a coin record in the blockchain. 
         /// Warning: This is barely useful, since it does not include spent coins!
         /// </summary>
-        public async static Task<GetCoinRecordByName_Response> GetCoinRecordByName_Async(GetCoinRecordByName_RPC name)
+        public async Task<GetCoinRecordByName_Response> GetCoinRecordByName_Async(GetCoinRecordByName_RPC name)
         {
             string response = await SendCustomMessage_Async("get_coin_record_by_name", name.ToString());
             GetCoinRecordByName_Response json = JsonSerializer.Deserialize<GetCoinRecordByName_Response>(response);
             return json;
         }
-        public async static Task<GetCoinRecordsByNames_Response> GetCoinRecordsByNames_Async(GetCoinRecordsByNames_RPC name)
+        public async Task<GetCoinRecordsByNames_Response> GetCoinRecordsByNames_Async(GetCoinRecordsByNames_RPC name)
         {
             string response = await SendCustomMessage_Async("get_coin_records_by_names", name.ToString());
             GetCoinRecordsByNames_Response json = JsonSerializer.Deserialize<GetCoinRecordsByNames_Response>(response);
