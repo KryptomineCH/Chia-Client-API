b79f306134a87d19f37d836196a8b83f14026126
Julian B <bechti44@googlemail.com>
2023-04-14 11:16:05 +0200

    updated xml documentation

--- Chia-Client-API/WalletAPI_NS/WalletApi_WalletManagement.cs
+++ Chia-Client-API/WalletAPI_NS/WalletApi_WalletManagement.cs
@@ -1,7 +1,6 @@
-﻿using CHIA_RPC.Wallet_NS.CATsAndTrading_NS;
-using CHIA_RPC.Wallet_NS.DID_NS;
-using CHIA_RPC.Wallet_NS.NFT_NS;
-using CHIA_RPC.Wallet_NS.WalletManagement_NS;
+﻿using CHIA_RPC.General_NS;
+using CHIA_RPC.Objects_NS;
+using CHIA_RPC.Wallet_NS.CATsAndTrading_NS;
 using System.Text.Json;
 
 namespace Chia_Client_API.WalletAPI_NS
@@ -9,131 +8,286 @@ namespace Chia_Client_API.WalletAPI_NS
     public partial class Wallet_RPC_Client
     {
         /// <summary>
-        /// returns all subwallets of the currently logged in wallet<br/><br/>
-        /// Note:  if you want to obtain all main wallets, use <see cref="GetPublicKeys_Async"/> instead
+        /// Rename a CAT wallet
         /// </summary>
-        /// <remarks><see href="https://docs.chia.net/wallet-rpc#get_wallets"/></remarks>
-        /// <param name="includeData">Set to true to include all coin info for this wallet</param>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#cat_set_name"/></remarks>
+        /// <param name="rpc"></param>
         /// <returns></returns>
-        public async Task<GetWallets_Response> GetWallets_Async(bool includeData = true)
+        public async Task<WalletID_Response> CatSetName_Async(CatSetName_RPC rpc)
         {
-            GetWallets_RPC rpc = new GetWallets_RPC { include_data= includeData };
-            string response = await SendCustomMessage_Async("get_wallets", rpc.ToString());
-            GetWallets_Response json = JsonSerializer.Deserialize<GetWallets_Response>(response);
-            return json;
+            string responseJson = await SendCustomMessage_Async("cat_set_name", rpc.ToString());
+            WalletID_Response deserializedObject = JsonSerializer.Deserialize<WalletID_Response>(responseJson);
+            return deserializedObject;
         }
         /// <summary>
-        /// returns all subwallets of the currently logged in wallet<br/><br/>
-        /// Note:  if you want to obtain all main wallets, use <see cref="GetPublicKeys_Sync"/> instead
+        /// Rename a CAT wallet
         /// </summary>
-        /// <remarks><see href="https://docs.chia.net/wallet-rpc#get_wallets"/></remarks>
-        /// <param name="includeData">Set to true to include all coin info for this wallet</param>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#cat_set_name"/></remarks>
+        /// <param name="rpc"></param>
         /// <returns></returns>
-        public GetWallets_Response GetWallets_Sync(bool includeData = true)
+        public WalletID_Response CatSetName_Sync(CatSetName_RPC rpc)
         {
-            Task<GetWallets_Response> data = Task.Run(() => GetWallets_Async(includeData));
+            Task<WalletID_Response> data = Task.Run(() => CatSetName_Async(rpc));
             data.Wait();
             return data.Result;
         }
         /// <summary>
-        /// create a new cat subwallet
+        /// Retrieve a CAT's name from its ID
         /// </summary>
-        /// <remarks><see href="https://docs.chia.net/wallet-rpc#create_new_wallet"/></remarks>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#cat_asset_id_to_name"/></remarks>
+        /// <param name="rpc"></param>
         /// <returns></returns>
-        public async Task<CreateNewWallet_Response> CreateNewCatWallet_Async(CreateNewCatWallet_RPC createNewCatWallet_RPC)
+        public async Task<CatAssetIdToName_Response> CatAssetIDToName_Async(CatAssetIdToName_RPC rpc)
         {
-            string response = await SendCustomMessage_Async("create_new_wallet ", createNewCatWallet_RPC.ToString());
-            CreateNewWallet_Response json = JsonSerializer.Deserialize<CreateNewWallet_Response>(response);
-            return json;
+            string responseJson = await SendCustomMessage_Async("cat_asset_id_to_name", rpc.ToString());
+            CatAssetIdToName_Response deserializedObject = JsonSerializer.Deserialize<CatAssetIdToName_Response>(responseJson);
+            return deserializedObject;
         }
         /// <summary>
-        /// create a new cat subwallet
+        /// Retrieve a CAT's name from its ID
         /// </summary>
-        /// <remarks><see href="https://docs.chia.net/wallet-rpc#create_new_wallet"/></remarks>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#cat_asset_id_to_name"/></remarks>
+        /// <param name="rpc"></param>
         /// <returns></returns>
-        public CreateNewWallet_Response CreateNewCatWallet_Sync(CreateNewCatWallet_RPC createNewCatWallet_RPC)
+        public CatAssetIdToName_Response CatAssetIDToName_Sync(CatAssetIdToName_RPC rpc)
         {
-            Task<CreateNewWallet_Response> data = Task.Run(() => CreateNewCatWallet_Async(createNewCatWallet_RPC));
+            Task<CatAssetIdToName_Response> data = Task.Run(() => CatAssetIDToName_Async(rpc));
             data.Wait();
             return data.Result;
         }
         /// <summary>
-        /// modify a cat wallet
+        /// Get the name of a CAT associated with a wallet ID
         /// </summary>
-        /// <remarks><see href="https://docs.chia.net/wallet-rpc#create_new_wallet"/></remarks>
-        /// <param name="modifyCatWallet_RPC"></param>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#cat_get_name"/></remarks>
+        /// <param name="rpc"></param>
         /// <returns></returns>
-        public async Task<CreateNewWallet_Response> ModifyCatWallet_Async(ModifyCatWallet_RPC modifyCatWallet_RPC)
+        public async Task<CatAssetIdToName_Response> CatGetName_Async(WalletID_RPC rpc)
         {
-            string response = await SendCustomMessage_Async("create_new_wallet ", modifyCatWallet_RPC.ToString());
-            CreateNewWallet_Response json = JsonSerializer.Deserialize<CreateNewWallet_Response>(response);
-            return json;
+            string responseJson = await SendCustomMessage_Async("cat_get_name", rpc.ToString());
+            CatAssetIdToName_Response deserializedObject = JsonSerializer.Deserialize<CatAssetIdToName_Response>(responseJson);
+            return deserializedObject;
         }
         /// <summary>
-        /// modify a cat wallet
+        /// Get the name of a CAT associated with a wallet ID
         /// </summary>
-        /// <remarks><see href="https://docs.chia.net/wallet-rpc#create_new_wallet"/></remarks>
-        /// <param name="modifyCatWallet_RPC"></param>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#cat_get_name"/></remarks>
+        /// <param name="rpc"></param>
         /// <returns></returns>
-        public CreateNewWallet_Response ModifyCatWallet_Sync(ModifyCatWallet_RPC modifyCatWallet_RPC)
+        public CatAssetIdToName_Response CatGetName_Sync(WalletID_RPC rpc)
         {
-            Task<CreateNewWallet_Response> data = Task.Run(() => ModifyCatWallet_Async(modifyCatWallet_RPC));
+            Task<CatAssetIdToName_Response> data = Task.Run(() => CatGetName_Async(rpc));
             data.Wait();
             return data.Result;
         }
         /// <summary>
-        /// create a new digital identity<br/><br/>
-        /// NOTE: Because backup_dids is required, you must already have access to a DID in order to run this RPC for a did_wallet.
-        /// If you do not already have a DID, then run the CLI command to create a DID wallet instead.
+        /// Get a list of all unacknowledged CATs
         /// </summary>
-        /// <remarks><see href="https://docs.chia.net/wallet-rpc#create_new_wallet"/></remarks>
-        /// <param name="createNewDIDWallet_RPC"></param>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#get_stray_cats"/></remarks>
         /// <returns></returns>
-        public async Task<CreateNewWallet_Response> CreateNewDidWallet_Async(CreateNewDIDWallet_RPC createNewDIDWallet_RPC)
+        public async Task<GetStrayCats_Response> GetStrayCats_Async()
         {
-            string response = await SendCustomMessage_Async("create_new_wallet ", createNewDIDWallet_RPC.ToString());
-            CreateNewWallet_Response json = JsonSerializer.Deserialize<CreateNewWallet_Response>(response);
-            return json;
+            string responseJson = await SendCustomMessage_Async("get_stray_cats");
+            GetStrayCats_Response deserializedObject = JsonSerializer.Deserialize<GetStrayCats_Response>(responseJson);
+            return deserializedObject;
         }
         /// <summary>
-        /// create a new digital identity<br/><br/>
-        /// NOTE: Because backup_dids is required, you must already have access to a DID in order to run this RPC for a did_wallet.
-        /// If you do not already have a DID, then run the CLI command to create a DID wallet instead.
+        /// Get a list of all unacknowledged CATs
         /// </summary>
-        /// <remarks><see href="https://docs.chia.net/wallet-rpc#create_new_wallet"/></remarks>
-        /// <param name="createNewDIDWallet_RPC"></param>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#get_stray_cats"/></remarks>
         /// <returns></returns>
-        public CreateNewWallet_Response CreateNewDidWallet_Sync(CreateNewDIDWallet_RPC createNewDIDWallet_RPC)
+        public GetStrayCats_Response GetStrayCats_Sync()
         {
-            Task<CreateNewWallet_Response> data = Task.Run(() => CreateNewDidWallet_Async(createNewDIDWallet_RPC));
+            Task<GetStrayCats_Response> data = Task.Run(() => GetStrayCats_Async());
             data.Wait();
             return data.Result;
         }
         /// <summary>
-        /// create a new nft wallet
+        /// Send CAT funds to another wallet
         /// </summary>
-        /// <remarks><see href="https://docs.chia.net/wallet-rpc#create_new_wallet"/></remarks>
-        /// <param name="name">eg My NFT WAllet</param>
-        /// <param name="didID">eg did:chia:1ypvxg7t327m4hsmgzrlhnuk4448nqc20crnnmzzd52lk7dvdza9s8qp8q6</param>
-        /// <param name="networkFeeMojos"> eg 1000</param>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#cat_spend"/></remarks>
+        /// <param name="rpc"></param>
         /// <returns></returns>
-        public async Task<CreateNewWallet_Response> CreateNewNftWallet_Async(CreateNewNFTWallet_RPC createNewNFTWallet_RPC)
+        public async Task<CatSpend_Response> CatSpend_Async(CatSpend_RPC rpc)
         {
-            string response = await SendCustomMessage_Async("create_new_wallet ", createNewNFTWallet_RPC.ToString());
-            CreateNewWallet_Response json = JsonSerializer.Deserialize<CreateNewWallet_Response>(response);
-            return json;
+            string responseJson = await SendCustomMessage_Async("cat_spend", rpc.ToString());
+            CatSpend_Response deserializedObject = JsonSerializer.Deserialize<CatSpend_Response>(responseJson);
+            return deserializedObject;
         }
         /// <summary>
-        /// create a new nft wallet
+        /// Send CAT funds to another wallet
         /// </summary>
-        /// <remarks><see href="https://docs.chia.net/wallet-rpc#create_new_wallet"/></remarks>
-        /// <param name="name">eg My NFT WAllet</param>
-        /// <param name="didID">eg did:chia:1ypvxg7t327m4hsmgzrlhnuk4448nqc20crnnmzzd52lk7dvdza9s8qp8q6</param>
-        /// <param name="networkFeeMojos"> eg 1000</param>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#cat_spend"/></remarks>
+        /// <param name="rpc"></param>
         /// <returns></returns>
-        public CreateNewWallet_Response CreateNewNftWallet_Sync(CreateNewNFTWallet_RPC createNewNFTWallet_RPC)
+        public CatSpend_Response CatSpend_Sync(CatSpend_RPC rpc)
         {
-            Task<CreateNewWallet_Response> data = Task.Run(() => CreateNewNftWallet_Async(createNewNFTWallet_RPC));
+            Task<CatSpend_Response> data = Task.Run(() => CatSpend_Async(rpc));
+            data.Wait();
+            return data.Result;
+        }
+        /// <summary>
+        /// Retrieve a the asset ID from a CAT wallet
+        /// </summary>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#cat_get_asset_id"/></remarks>
+        /// <param name="rpc"></param>
+        /// <returns></returns>
+        public async Task<CatGetAssetId_Response> CatGetAssetID_Async(WalletID_RPC rpc)
+        {
+            string responseJson = await SendCustomMessage_Async("cat_get_asset_id", rpc.ToString());
+            CatGetAssetId_Response deserializedObject = JsonSerializer.Deserialize<CatGetAssetId_Response>(responseJson);
+            return deserializedObject;
+        }
+        /// <summary>
+        /// Retrieve a the asset ID from a CAT wallet
+        /// </summary>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#cat_get_asset_id"/></remarks>
+        /// <param name="rpc"></param>
+        /// <returns></returns>
+        public CatGetAssetId_Response CatGetAssetID_Sync(WalletID_RPC rpc)
+        {
+            Task<CatGetAssetId_Response> data = Task.Run(() => CatGetAssetID_Async(rpc));
+            data.Wait();
+            return data.Result;
+        }
+        /// <summary>
+        /// Create a new offer<br/><br/>
+        /// WARNING: Due to missing documentation may be implemented incorrectly
+        /// </summary>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#create_offer_for_ids"/></remarks>
+        /// <param name="rpc"></param>
+        /// <returns></returns>
+        public async Task<OfferFile> CreateOfferForIDs_Async(CreateOfferForIds_RPC rpc)
+        {
+            string responseJson = await SendCustomMessage_Async("create_offer_for_ids", rpc.ToString());
+            OfferFile deserializedObject = JsonSerializer.Deserialize<OfferFile>(responseJson);
+            return deserializedObject;
+        }
+        /// <summary>
+        /// Create a new offer<br/><br/>
+        /// WARNING: Due to missing documentation may be implemented incorrectly
+        /// </summary>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#create_offer_for_ids"/></remarks>
+        /// <param name="rpc"></param>
+        /// <returns></returns>
+        public OfferFile CreateOfferForIDs_Sync(CreateOfferForIds_RPC rpc)
+        {
+            Task<OfferFile> data = Task.Run(() => CreateOfferForIDs_Async(rpc));
+            data.Wait();
+            return data.Result;
+        }
+        /// <summary>
+        /// Show a summary of an offer
+        /// </summary>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#get_offer_summary"/></remarks>
+        /// <param name="rpc"></param>
+        /// <returns></returns>
+        public async Task<GetCatOfferSummary_Response> GetOfferSummary_Async(GetCatOfferSummary_RPC rpc)
+        {
+            string responseJson = await SendCustomMessage_Async("get_offer_summary", rpc.ToString());
+            GetCatOfferSummary_Response deserializedObject = JsonSerializer.Deserialize<GetCatOfferSummary_Response>(responseJson);
+            return deserializedObject;
+        }
+        /// <summary>
+        /// Show a summary of an offer
+        /// </summary>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#get_offer_summary"/></remarks>
+        /// <param name="rpc"></param>
+        /// <returns></returns>
+        public GetCatOfferSummary_Response GetOfferSummary_Sync(GetCatOfferSummary_RPC rpc)
+        {
+            Task<GetCatOfferSummary_Response> data = Task.Run(() => GetOfferSummary_Async(rpc));
+            data.Wait();
+            return data.Result;
+        }
+        /// <summary>
+        /// Take an offer
+        /// </summary>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#take_offer"/></remarks>
+        /// <param name="rpc"></param>
+        /// <returns></returns>
+        public async Task<TradeRecord_Response> TakeOffer_Async(TakeCatOffer_RPC rpc)
+        {
+            string responseJson = await SendCustomMessage_Async("take_offer", rpc.ToString());
+            TradeRecord_Response deserializedObject = JsonSerializer.Deserialize<TradeRecord_Response>(responseJson);
+            return deserializedObject;
+        }
+        /// <summary>
+        /// Take an offer
+        /// </summary>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#take_offer"/></remarks>
+        /// <param name="rpc"></param>
+        /// <returns></returns>
+        public TradeRecord_Response TakeOffer_Sync(TakeCatOffer_RPC rpc)
+        {
+            Task<TradeRecord_Response> data = Task.Run(() => TakeOffer_Async(rpc));
+            data.Wait();
+            return data.Result;
+        }
+        /// <summary>
+        /// Obtain the number of offers from the current wallet
+        /// </summary>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#get_offers_count"/></remarks>
+        /// <returns></returns>
+        public async Task<GetOffersCount_Response> GetOffersCount_Async()
+        {
+            string responseJson = await SendCustomMessage_Async("get_offers_count");
+            GetOffersCount_Response deserializedObject = JsonSerializer.Deserialize<GetOffersCount_Response>(responseJson);
+            return deserializedObject;
+        }
+        /// <summary>
+        /// Obtain the number of offers from the current wallet
+        /// </summary>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#get_offers_count"/></remarks>
+        /// <returns></returns>
+        public GetOffersCount_Response GetOffersCount_Sync()
+        {
+            Task<GetOffersCount_Response> data = Task.Run(() => GetOffersCount_Async());
+            data.Wait();
+            return data.Result;
+        }
+        /// <summary>
+        /// Cancel all offers, with the option to cancel only offers for a specific asset class
+        /// </summary>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#cancel_offers"/></remarks>
+        /// <param name="rpc"></param>
+        /// <returns></returns>
+        public async Task<Success_Response> CancelOffers_Async(CancelCatOffers_RPC rpc)
+        {
+            string responseJson = await SendCustomMessage_Async("cancel_offers", rpc.ToString());
+            Success_Response deserializedObject = JsonSerializer.Deserialize<Success_Response>(responseJson);
+            return deserializedObject;
+        }
+        /// <summary>
+        /// Cancel all offers, with the option to cancel only offers for a specific asset class
+        /// </summary>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#cancel_offers"/></remarks>
+        /// <param name="rpc"></param>
+        /// <returns></returns>
+        public Success_Response CancelOffers_Sync(CancelCatOffers_RPC rpc)
+        {
+            Task<Success_Response> data = Task.Run(() => CancelOffers_Async(rpc));
+            data.Wait();
+            return data.Result;
+        }
+        /// <summary>
+        /// Return the default CAT list
+        /// </summary>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#get_cat_list"/></remarks>
+        /// <returns></returns>
+        public async Task<GetCatList_Response> GetCatList_Async()
+        {
+            string responseJson = await SendCustomMessage_Async("get_cat_list");
+            GetCatList_Response deserializedObject = JsonSerializer.Deserialize<GetCatList_Response>(responseJson);
+            return deserializedObject;
+        }
+        /// <summary>
+        /// Return the default CAT list
+        /// </summary>
+        /// <remarks><see href="https://docs.chia.net/wallet-rpc#get_cat_list"/></remarks>
+        /// <returns></returns>
+        public GetCatList_Response GetCatList_Sync()
+        {
+            Task<GetCatList_Response> data = Task.Run(() => GetCatList_Async());
             data.Wait();
             return data.Result;
         }
