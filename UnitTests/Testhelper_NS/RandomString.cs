8382053c6b09257c4b49c8a1128de2d0c76da885
Julian B <bechti44@googlemail.com>
2023-04-21 09:25:23 +0200

    added random string function for test purposes

---
+++ UnitTests/Testhelper_NS/RandomString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace CHIA_API_Tests.Testhelper_NS
+{
+    internal class RandomString
+    {
+        public static string GenerateRandomString(int length)
+        {
+            const string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            StringBuilder result = new StringBuilder(length);
+            Random random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                result.Append(characters[random.Next(characters.Length)]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
