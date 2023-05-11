4e1bf5a3809becc30079196ec51c4231e6a23efb
Julian B <bechti44@googlemail.com>
2023-05-11 10:57:18 +0200

    added further full node tests

--- UnitTests/FullNode_NS/FullNode_Tests.cs
+++ UnitTests/FullNode_NS/FullNode_Tests.cs
@@ -26,5 +26,33 @@ namespace CHIA_API_Tests.FullNode_NS
 
             { }
         }
+        [Fact]
+        public void GetCoinRecordByPuzzhashes_Test()
+        {
+            GetCoinRecordsByPuzzleHashes_RPC rpc = new GetCoinRecordsByPuzzleHashes_RPC(
+                puzzle_hashes: new string[] {
+                "0x3d55fb3762b6db45add3942a3f8ce35be981092e385c7a0f4fdcdbbd5c26d273",
+                "0x3440784feb92f41a0b0d85f2a60b2319cb323372775bdf223b7042479f41c5cd",
+                "0x2832b7d8e2aefd9d49592c134b50fb8d83607122ac5c9219b404b99596ea311d"
+                }, include_spent_coins: true);
+
+            GetCoinRecords_Response test1 = Testnet_FullNode.Fullnode_Client.GetCoinRecordsByPuzzleHashes_Sync(rpc);
+            { }
+        }
+        [Fact]
+        public void GetCoinRecordsByNames_Test()
+        {
+            GetCoinRecordsByNames_RPC rpc = new GetCoinRecordsByNames_RPC()
+            {
+                names= new string[] {
+                    "0x2832b7d8e2aefd9d49592c134b50fb8d83607122ac5c9219b404b99596ea311d",
+                    "0x2bc1f5e8240e6bcbf4ffb3d4d590761612fe98986fb4db1fd1948de2cc65a7e5"
+                },
+                include_spent_coins=true
+            };
+
+            GetCoinRecords_Response test1 = Testnet_FullNode.Fullnode_Client.GetCoinRecordsByNames_Sync(rpc);
+            { }
+        }
     }
 }
