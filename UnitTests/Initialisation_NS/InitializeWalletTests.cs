001082be37fd4d598f55e2db3a45b971f4318da3
Julian B <bechti44@googlemail.com>
2023-12-21 20:45:02 +0100

    removed amount_correct_custom as it was fixed with chia 2.1.2

--- UnitTests/Initialisation_NS/InitializeWalletTests.cs
+++ UnitTests/Initialisation_NS/InitializeWalletTests.cs
@@ -22,7 +22,7 @@ namespace CHIA_API_Tests.Initialisation_NS
                 Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
                 ".testnet","ssl");
                 Wallet_Client = new WalletRpcClient(reportResponseErrors: false, targetApiAddress: "192.168.1.117", targetCertificateBaseFolder: certificatePath);
-                Wallet_WebSocket_Client = new WalletWebSocketClient(reportResponseErrors: false, targetApiAddress: "192.168.1.117", targetCertificateBaseFolder: certificatePath);
+                //Wallet_WebSocket_Client = new WalletWebSocketClient(reportResponseErrors: false, targetApiAddress: "192.168.1.117", targetCertificateBaseFolder: certificatePath);
                 // close all old wallets (if more than 1 are open)
                 GetPublicKeys_Response? pre_wallets = Wallet_Client.GetPublicKeys_Async().Result;
                 Assert.NotNull(pre_wallets);
